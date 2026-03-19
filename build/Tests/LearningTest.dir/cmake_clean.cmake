c42c7f86905dee3957f46225724893cc0edaaa2a
Aayush <81830617+JustA3DObject@users.noreply.github.com>
2026-03-19 11:54:33 +0530

    Add files via upload

---
+++ build/Tests/LearningTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearningTest.dir/LearningTest/LearningTestManager.cpp.o"
+  "CMakeFiles/LearningTest.dir/LearningTest/LearningTestManager.cpp.o.d"
+  "CMakeFiles/LearningTest.dir/LearningTest/main.cpp.o"
+  "CMakeFiles/LearningTest.dir/LearningTest/main.cpp.o.d"
+  "LearningTest"
+  "LearningTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearningTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Tests/SlidingTest.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SlidingTest.dir/SlidingTest/SlidingTestApp.cpp.o"
+  "CMakeFiles/SlidingTest.dir/SlidingTest/SlidingTestApp.cpp.o.d"
+  "CMakeFiles/SlidingTest.dir/SlidingTest/SlidingTestManager.cpp.o"
+  "CMakeFiles/SlidingTest.dir/SlidingTest/SlidingTestManager.cpp.o.d"
+  "CMakeFiles/SlidingTest.dir/SlidingTest/main.cpp.o"
+  "CMakeFiles/SlidingTest.dir/SlidingTest/main.cpp.o.d"
+  "SlidingTest"
+  "SlidingTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SlidingTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
